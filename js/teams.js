80fda7a5cb2851b93648a5486babce0f3015b962
seba-1511 <seba-1511@hotmail.com>
2012-12-24 18:01:20 +0100

    Started the login process.

--- js/teams.js
+++ js/teams.js
@@ -1,11 +1,35 @@
 //Announcement of the Namespace
 var tooskiTeams = {
 	
+	login: function() {
+		
+	},
+	
+	loadLoginPage: function() {
+		$.mobile.changePage('html/login.html', {
+			role: 'dialog'
+		});
+	},
+	
+	loggedIn: function() {
+		return false;
+	},
+	
 	/*
 	 * The first function to be called when the app is started.
 	 */
 	init: function() {
-		
+		if (this.loggedIn()) {
+			if (this.hasTeamSettings()) {
+				this.loadWelcomePage();
+			}
+			else {
+				this.getTeamsSettings();
+			}
+		}
+		else {
+			this.loadLoginPage();
+		}
 	},
 	
 	/*
@@ -13,11 +37,6 @@ var tooskiTeams = {
 	 * and then execute a specific function.
 	 */
 	change: function(page, functionToCall) {
-		/*
-		 $.mobile.changePage('html/'+page+'.html', {
-			transition: 'pop'
-		});
-		*/
 		$('#content').load('html/'+page+'.html', function() {
 			if (functionToCall) {
 					functionToCall();
