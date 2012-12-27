8cb63c7afde82e7895c387746004180c5132d739
seba-1511 <seba-1511@hotmail.com>
2012-12-27 19:54:53 +0100

    IMplemented Menu and loading the teams.(Day 3)

--- js/teams.js
+++ js/teams.js
@@ -1,8 +1,9 @@
 //Announcement of the Namespace
 var tooskiTeams = {
 	//TODO: Change this line:
-	ServerUrl: 'http://localhost/tooski/api/',
+	ServerUrl: 'http://192.168.1.103/tooski/api/',
 	storage: localStorage,
+	db: null,
 	
 	
 	//TODO: Implement function:
@@ -58,26 +59,92 @@ var tooskiTeams = {
 	},
 	
 	loadLoginPage: function() {
-		$.mobile.changePage('html/login.html', {
+		$.mobile.loadPage('html/login.html', {
 			role: 'dialog'
+		}).done(function() {
+			$('div[data-url="/tooski_mobile/html/login.html"]').attr('id', 'loginPage');
+			$.mobile.changePage('#loginPage');
+			$('div[data-role="header"] > a[data-icon="delete"]').hide();
 		});
+	},
+	
+	generateTeamMenu: function() {
+		$('#panel').html('<h3 style="margin-bottom:0px;margin-top:10px;margin-left:15px;" align="center">Teams</h3><hr style="maring-left:5px;" align="center" height="10px" width="90%" /><hr color="black" size="1px" width="100%" style="margin-bottom:10px;">');
+		this.db.transaction(function(tx) {
+			tx.executeSql('SELECT * FROM teams',
+			[],
+			function(tx, rs){
+				var html = '';
+				for (var i=0; i < rs.rows.length; i++) {
+					html += '<div style="padding-left:5px;border-bottom:solid black 1px;padding-top:0px;padding-bottom:0px;margin-top:0px;margin-bottom:0px;"><h4 style="padding-top:5px;padding-bottom:4px;margin-top:5px;margin-bottom:4px;"><img src="'+rs.rows.item(i).logo+'" style="max-height:30px;max-width:75px;margin-right:10px;vertical-align:middle;" />'+rs.rows.item(i).name+'</h4></div>';
+				}
+				$('#panel').append(html);
+			},
+			tooskiTeams.dbError
+			);
+		});
+	},
+	
+	dbError: function() {
+		alert('Problème lors de la connexion à la base de donnée. Veuillez relancer l\'app en cas de récidive.');
+	},
+	
+	getWelcomePage: function() {
+		console.log('WelcomePage');
 	},
 	
 	loggedIn: function() {
 		return (this.storage.user && this.storage.key && this.storage.keyId);
+	},
+	//TODO: Implement with this.makeRequest and server side.
+	getTeamFromServer: function() {
+		return '{"length":3, "name":["Tooski Team", "Ski-Romand Junior", "Guggen"],"id":["6", "10", "11"], "logo":["http://tooski.ch/assets/uploads/files/header.png", "http://www.ski-romand.ch/themes/skiromand-2/templates/logo.png", "http://seba1511.com/LogoTooskiTeam/image.php?w=187&h=73&t=Guggenmusik"], "email":["info@tooski.ch", "pokerstar1511@gmail.com", "test@test.com"] }';
+	},
+	
+	storeTeamsInDatabase: function(teamJson) {
+		var team = $.parseJSON(teamJson);
+		this.db.transaction(function(tx){
+			for (var i=0; i<team.length; i++) {
+				tx.executeSql('INSERT INTO teams(id, name, logo, email) VALUES (?, ?, ?, ?)',
+					[team.id[i], team.name[i], team.logo[i], team.email[i]],
+					function() {},
+					this.dbError
+				);
+			}
+		});
+		this.storage.hasTeamSettings = true;
+	},
+	
+	initializeDatabase: function() {
+		this.db = openDatabase('teamDB', '1.0', 'The Tooski Team App Database', 65536);
+		this.db.transaction(function(tx){
+			tx.executeSql('CREATE TABLE IF NOT EXISTS teams(id INTEGER PRIMARY KEY ASC, name TEXT, logo TEXT, email TEXT)', [],function(){} ,this.dbError);
+		});
+	},
+	
+	getTeamSettings: function() {
+		var teamJson = this.getTeamFromServer();
+		this.initializeDatabase();
+		this.storeTeamsInDatabase(teamJson);
+	},
+	
+	hasTeamSettings: function() {
+		return false;//this.storage.hasTeamSettings;//check in db !
 	},
 	
 	/*
 	 * The first function to be called when the app is started.
 	 */
 	init: function() {
+		if (!($.mobile.activePage[0].id == 'page')) {
+			$.mobile.changePage('index.html');
+		}
 		if (this.loggedIn()) {
-			if (this.hasTeamSettings()) {
-				this.loadWelcomePage();
+			if (!this.hasTeamSettings()) {
+				this.getTeamSettings();
 			}
-			else {
-				this.getTeamsSettings();
-			}
+			this.generateTeamMenu();
+			this.getWelcomePage();
 		}
 		else {
 			this.loadLoginPage();
